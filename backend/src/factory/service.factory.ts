3250a5e8743993f4420c02da0928c459021753cd
Henry Huynh <henry@covr.cc>
2026-05-29 20:44:13 +1000

    Updated service factory with added services

--- backend/src/factory/service.factory.ts
+++ backend/src/factory/service.factory.ts
@@ -1,9 +1,17 @@
 import { AuthService } from "../services/auth.service";
 import { UserService } from "../services/user.service";
 import { EnvironmentService } from "../services/environment.service";
+import { WeatherService } from "../services/weather.service";
+import { HttpService } from "../services/http.service";
+import { MessageService } from "../services/message.service";
 
-const userService = new UserService();
-const environmentService = new EnvironmentService();
-
-export const createAuthService = () =>
-  new AuthService(environmentService, userService);
+export const createEnvironmentService = () => new EnvironmentService();
+export const createUserService = () => new UserService();
+export const createHttpService = () => new HttpService();
+export const createMessageService = () => new MessageService();
+export const createWeatherService = (httpService: HttpService) =>
+  new WeatherService(httpService);
+export const createAuthService = (
+  environmentService: EnvironmentService,
+  userService: UserService,
+) => new AuthService(environmentService, userService);
