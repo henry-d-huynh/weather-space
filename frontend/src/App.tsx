d1163123c35341674f4a172716bfb73510180428
Henry Huynh <henry@covr.cc>
2026-05-30 21:49:58 +1000

    Fixed spelling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { useAppSelector } from "./store/hooks";
 import { authSlice } from "./features/auth/store/auth.slice";
 import { LOADABLE_STATUS } from "./types/loadable.type";
+import { useWebSocket } from "./hooks/useWebSocket";
 
 export const App = () => {
   const authState = useAppSelector((state) =>
     authSlice.selectors.getAuth(state.auth),
   );
+  const { subscribeToCity } = useWebSocket();
 
   if (authState.status !== LOADABLE_STATUS.LOADED) {
     return <div>Login page goes here</div>;
