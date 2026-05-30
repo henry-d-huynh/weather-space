99d897c8d192ac00e99e8ea9a64619e4a2e50738
Henry Huynh <henry@covr.cc>
2026-05-30 17:21:42 +1000

    Removes error throw bug with env vars

--- frontend/src/utility/environment.utility.ts
+++ frontend/src/utility/environment.utility.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  apiUrl:
-    import.meta.env.VITE_API_URL.toString() ?? "http://localhost:3000/api",
-  wsUrl: import.meta.env.VITE_WS_URL.toString() ?? "ws://localhost:3000",
+  apiUrl: import.meta.env.VITE_API_URL ?? "http://localhost:3000/api",
+  wsUrl: import.meta.env.VITE_WS_URL ?? "ws://localhost:3000",
 };
